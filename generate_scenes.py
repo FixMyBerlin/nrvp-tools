1a8552a070904a20de1a85a88d05879011fa6935
Vincent Ahrend <mail@vincentahrend.com>
2019-11-11 12:13:52 +0100

    Add rules for scenario 4

--- generate_scenes.py
+++ generate_scenes.py
@@ -1,12 +1,37 @@
 import csv
 import os
+import sys
 import random
 from PIL import Image
 from pprint import pprint
 
 IMAGE_SIZE = (1280, 720)
 
-layer_names = ["blumenkasten", "01_MS_C__0019_Basis"]
+layers_used = set()
+
+layer_names = [
+    "01_MS_C__0000_Lenker",
+    "01_MS_C__0001_HVS-04-Trli-baulTrennung-Sperrpfosten_hoch_B",
+    "01_MS_C__0002_FS-Aufkommen-hochA",
+    "01_MS_C__0003_FS-Aufkommen-hochB",
+    "01_MS_C__0004_FS-Aufkommen-hochC",
+    "01_MS_C__0005_HVS-04-Trli-baulTrennung-Sperrpfosten_hochA",
+    "01_MS_C__0006_HVS-04-Trli-Breit-Pflanzenbeete",
+    "01_MS_C__0007_HVS-04-Trli-baulTrennung-Sperrpfosten-niedrig",
+    "01_MS_C__0008_TrRe__SchmalDurchgezogen",
+    "01_MS_C__0009_Tempo30",
+    "01_MS_C__0010_Tempo50",
+    "01_MS_C__0011_HVS-04-Trli-Schmal-unterbrochen",
+    "01_MS_C__0012_HVS-04-Trli-Schmal-durchgezogen",
+    "01_MS_C__0013_HVS-04-Trli-Breit-Doppellinie",
+    "01_MS_C__0014_HVS-04-Trli-Breit-schraffiert",
+    "01_MS_C__0015_RVA-Farbig-Schraffur",
+    "01_MS_C__0016_RVA-Farbig_abgeschnitten (1)",
+    "01_MS_C__0017_RVA-Farbig_ganz",
+    "01_MS_C__0018_HVS-04-Einbahnstraße",
+    "01_MS_C__0019_Basis",
+]
+csv.field_size_limit(sys.maxsize)
 
 
 def merge_images(images):
@@ -21,103 +46,118 @@ def merge_images(images):
     return rv.convert("RGB")
 
 
-def save_composite_image(layers, sceneID):
-    handles = [
-        Image.open(os.path.join("./layers/HVS-04_C_Test", layer + ".png"))
-        for layer in reversed(layers)
-    ]
-    image = merge_images(handles)
+def save_composite_image(layers, handles, sceneID):
+    image = merge_images([handles[layer] for layer in reversed(layers)])
     new_fname = os.path.join("./out", sceneID + ".jpg")
     image = image.resize(IMAGE_SIZE, Image.ANTIALIAS)
     image.save(new_fname, quality=60)
-    for handle in handles:
-        handle.close()
 
 
 def needs_layer(data, lname):
     if lname.endswith("Basis"):
         return True
 
-    if lname == "01_MS_C__0000_HVS-04-Trli-bauliTrennung-Sperrpfosten-hochB":
-        if data["Tr_li-Art"] == "Aufpflasterung":
-            return True
+    if "Trli-baulTrennung-Sperrpfosten_hoch" in lname:
+        return data["Tr_li-baulTrennung"] == "Sperrpfosten_hoch"
 
-    if lname == "01_MS_C__0001_FS-Aufkommen-hochA":
-        pass
+    if "Trli-baulTrennung-Sperrpfosten-niedrig" in lname:
+        return data["Tr_li-baulTrennung"] == "Sperrpfosten_niedrig"
 
-    if lname == "01_MS_C__0002_FS-Aufkommen-hochB":
-        pass
+    if "FS-Aufkommen-hoch" in lname:
+        return data["FS-Aufkommen"] == "hoch"
 
-    if lname == "01_MS_C__0003_FS-Aufkommen-hochC":
-        pass
+    if "Trli-Breit-Pflanzenbeete" in lname:
+        return data["Tr_li-baulTrennung"] == "Blumenkasten"
 
-    if lname == "01_MS_C__0004_HVS-04-Trli-bauliTrennung-Sperrpfosten-hochA":
-        pass
+    if "TrRe__SchmalDurchgezogen" in lname:
+        return False
 
-    if lname == "01_MS_C__0005_HVS-04-Trli-Breit-Pflanzenbeete":
-        pass
+    if "Tempo30" in lname:
+        return data["FS-Geschwindigkeit"] == "30"
 
-    if lname == "01_MS_C__0006_HVS-04-Trli-bauliTrennung-Sperrpfosten-niedrig":
-        pass
+    if "Tempo50" in lname:
+        return data["FS-Geschwindigkeit"] == "50"
 
-    if lname == "01_MS_C__0007_TrRe__SchmalDurchgezogen":
-        pass
+    if "Trli-Schmal-unterbrochen" in lname:
+        return data["Tr_li-Markierung"] == "unterbrochen"
 
-    if lname == "01_MS_C__0008_Tempo30":
-        pass
+    if "Trli-Schmal-durchgezogen" in lname:
+        return data["Tr_li-Markierung"] == "durchgezogen"
 
-    if lname == "01_MS_C__0009_Tempo50":
-        pass
+    if "Trli-Breit-Doppellinie" in lname:
+        return data["Tr_li-Markierung"] == "Doppellinie"
 
-    if lname == "01_MS_C__0010_HVS-04-Trli-Schmal-unterbrochen":
-        pass
+    if "Trli-Breit-schraffiert" in lname:
+        return data["Tr_li-Markierung"] == "Schraffiert"
 
-    if lname == "01_MS_C__0011_HVS-04-Trli-Schmal-durchgezogen":
-        pass
+    if "RVA-Farbig-Schraffur" in lname:
+        return data["RVA-Oberfläche"] == "Asphalt - farbig Schraffur"
 
-    if lname == "01_MS_C__0012_HVS-04-Trli-Breit-Doppellinie":
-        pass
+    if "RVA-Farbig_ganz" in lname:
+        return (
+            data["RVA-Oberfläche"] == "Asphalt - farbig"
+            and data["Tr_li-Breite"] == "schmal"
+        )
 
-    if lname == "01_MS_C__0013_HVS-04-Trli-Breit-schraffiert":
-        pass
+    if "RVA-Farbig_abgeschnitten" in lname:
+        return (
+            data["RVA-Oberfläche"] == "Asphalt - farbig"
+            and data["Tr_li-Breite"] == "breit"
+        )
 
-    if lname == "01_MS_C__0014_RVA-Farbig-Schraffur":
-        pass
+    if "Einbahnstraße" in lname:
+        return data["FS-Art"] == "Einbahn"
 
-    if lname == "01_MS_C__0015_RVA-Farbig":
-        pass
+    if "Tram" in lname:
+        return data["FS-Art"] == "Tram"
 
-    if lname == "01_MS_C__0016_Kurven-1":
-        pass
-
-    if lname == "01_MS_C__0017_HVS-04-Einbahnstraße":
-        pass
-
-    if lname == "01_MS_C__0018_Kurven-1-Kopie":
-        pass
-
-    if lname == "01_MS_C__0019_Basis":
-        pass
+    if "Lenker" in lname:
+        return data["Kamera"] == "C"
 
     return False
 
 
 def make_images(rows):
     total = len(rows)
+    # print(rows[0])
+
+    handles = {}
+    for lname in layer_names:
+        try:
+            handles[lname] = Image.open(
+                os.path.join("./layers/HVS-04_C_Test", lname + ".png")
+            )
+        except FileNotFoundError as e:
+            print("Error loading layer", e)
+            sys.exit()
+
     for i, row in enumerate(rows):
         print("{} / {}: {}".format(i, total, row["SceneID"]))
+        print(row)
         layers = [lname for lname in layer_names if needs_layer(row, lname)]
-        save_composite_image(layers, row["SceneID"])
+        print("\n - ".join(layers))
+        for layer in layers:
+            layers_used.add(layer)
+        save_composite_image(layers, handles, row["SceneID"])
+
+    for lname in layer_names:
+        handles[lname].close()
 
 
 def main():
-    with open("tabelle.csv") as f:
+    with open("Szenarienübersicht_HVS.csv") as f:
         csv_reader = csv.DictReader(f, delimiter=",")
-        rows = [row for row in csv_reader if row["Basisszenario"] == "4"]
+        rows = [
+            row
+            for row in csv_reader
+            if (row["Basisszenario"] == "4" and row["Kamera"] == "C")
+        ]
 
-    make_images(rows[:10])
+    make_images(rows)
 
-    # im = Image.open("")
+    print(
+        "Unused: {}".format(", ".join([l for l in layer_names if l not in layers_used]))
+    )
 
 
 if __name__ == "__main__":
