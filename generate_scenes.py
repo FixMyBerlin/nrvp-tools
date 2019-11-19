dd82ae081b75d3ace38ba1a3a7d0920caa885f28
Vincent Ahrend <mail@vincentahrend.com>
2019-11-19 18:32:32 +0100

    Let generated handle multiple base scenarios

--- generate_scenes.py
+++ generate_scenes.py
@@ -2,35 +2,18 @@ import csv
 import os
 import sys
 import random
-from PIL import Image
+from PIL import Image, ImageOps
+from collections import defaultdict
 from pprint import pprint
 
-IMAGE_SIZE = (1280, 720)
+IMAGE_SIZE = (1240, 930)
+IGNORE = (".DS_Store", "Icon\r")
 
 layers_used = set()
+missing_base = set()
+layer_ids = set()
+layersets = defaultdict(list)
 
-layer_names = [
-    "01_MS_C__0000_Lenker",
-    "01_MS_C__0001_HVS-04-Trli-baulTrennung-Sperrpfosten_hoch_B",
-    "01_MS_C__0002_FS-Aufkommen-hochA",
-    "01_MS_C__0003_FS-Aufkommen-hochB",
-    "01_MS_C__0004_FS-Aufkommen-hochC",
-    "01_MS_C__0005_HVS-04-Trli-baulTrennung-Sperrpfosten_hochA",
-    "01_MS_C__0006_HVS-04-Trli-Breit-Pflanzenbeete",
-    "01_MS_C__0007_HVS-04-Trli-baulTrennung-Sperrpfosten-niedrig",
-    "01_MS_C__0008_TrRe__SchmalDurchgezogen",
-    "01_MS_C__0009_Tempo30",
-    "01_MS_C__0010_Tempo50",
-    "01_MS_C__0011_HVS-04-Trli-Schmal-unterbrochen",
-    "01_MS_C__0012_HVS-04-Trli-Schmal-durchgezogen",
-    "01_MS_C__0013_HVS-04-Trli-Breit-Doppellinie",
-    "01_MS_C__0014_HVS-04-Trli-Breit-schraffiert",
-    "01_MS_C__0015_RVA-Farbig-Schraffur",
-    "01_MS_C__0016_RVA-Farbig_abgeschnitten (1)",
-    "01_MS_C__0017_RVA-Farbig_ganz",
-    "01_MS_C__0018_HVS-04-Einbahnstraße",
-    "01_MS_C__0019_Basis",
-]
 csv.field_size_limit(sys.maxsize)
 
 
@@ -47,14 +30,14 @@ def merge_images(images):
 
 
 def save_composite_image(layers, handles, sceneID):
-    image = merge_images([handles[layer] for layer in reversed(layers)])
+    image = merge_images([handles[layer] for layer in layers])
     new_fname = os.path.join("./out", sceneID + ".jpg")
-    image = image.resize(IMAGE_SIZE, Image.ANTIALIAS)
-    image.save(new_fname, quality=60)
+    image_cropped = ImageOps.fit(image, IMAGE_SIZE, centering=(0.5, 0.5))
+    image_cropped.save(new_fname, quality=60)
 
 
 def needs_layer(data, lname):
-    if lname.endswith("Basis"):
+    if lname.endswith("Basis.png"):
         return True
 
     if "Trli-baulTrennung-Sperrpfosten_hoch" in lname:
@@ -117,27 +100,55 @@ def needs_layer(data, lname):
     return False
 
 
-def make_images(rows):
+def get_layer_names(experiment, base, perspective):
+    basepath = os.path.abspath(
+        "./layers/{}/{:02d}/{}".format(experiment, int(base), perspective)
+    )
+
+    layer_names = [
+        f
+        for f in os.listdir(basepath)
+        if f not in IGNORE and os.path.isfile(os.path.join(basepath, f))
+    ]
+    for layer in layer_names:
+        layer_ids.add(layer.split("_", 5)[-1])
+
+    return basepath, layer_names
+
+
+def make_images(experiment, base, perspective, rows):
     total = len(rows)
-    # print(rows[0])
+    try:
+        basepath, layer_names = get_layer_names(experiment, base, perspective)
+    except FileNotFoundError as e:
+        return
 
     handles = {}
     for lname in layer_names:
         try:
-            handles[lname] = Image.open(
-                os.path.join("./layers/HVS-04_C_Test", lname + ".png")
-            )
+            handles[lname] = Image.open(os.path.join(basepath, lname))
         except FileNotFoundError as e:
             print("Error loading layer", e)
             sys.exit()
 
     for i, row in enumerate(rows):
-        print("{} / {}: {}".format(i, total, row["SceneID"]))
-        print(row)
-        layers = [lname for lname in layer_names if needs_layer(row, lname)]
-        print("\n - ".join(layers))
+        if (i + 1) % 25 == 0:
+            print("{} / {}: {}".format(i + 1, total, row["SceneID"]))
+        # pprint(row)
+        layers = sorted(
+            [lname for lname in layer_names if needs_layer(row, lname)], reverse=True
+        )
+
+        if len(layers) == 0:
+            return
+
         for layer in layers:
             layers_used.add(layer)
+
+        lset = frozenset(layers)
+        # if lset in layersets:
+        #     print(f"Layerset for Scene {row['SceneID']} already used in Scene {layersets[lset]}\n{lset}")
+        layersets[lset].append(row["SceneID"])
         save_composite_image(layers, handles, row["SceneID"])
 
     for lname in layer_names:
@@ -145,19 +156,46 @@ def make_images(rows):
 
 
 def main():
-    with open("Szenarienübersicht_HVS.csv") as f:
-        csv_reader = csv.DictReader(f, delimiter=",")
-        rows = [
-            row
-            for row in csv_reader
-            if (row["Basisszenario"] == "4" and row["Kamera"] == "C")
-        ]
+    experiments = ["HVS", "SR"]
+    for experiment in experiments:
+        with open("Szenarienübersicht_{}.csv".format(experiment)) as f:
+            csv_reader = csv.DictReader(f, delimiter=",")
+            rows = [row for row in csv_reader]
+        total = len(rows)
+        scenes_grouped = defaultdict(lambda: defaultdict(list))
+        for row in rows:
+            scenes_grouped[row["Basisszenario"]][row["Kamera"]].append(row)
 
-    make_images(rows)
+        for base in scenes_grouped.keys():
+            for perspective in scenes_grouped[base].keys():
+                print(
+                    "\n{}\tSzenen für Experiment {}\tBasisszenario {}\tPerspektive {}".format(
+                        len(scenes_grouped[base][perspective]),
+                        experiment,
+                        base,
+                        perspective,
+                    )
+                )
+                make_images(
+                    experiment, base, perspective, scenes_grouped[base][perspective]
+                )
 
-    print(
-        "Unused: {}".format(", ".join([l for l in layer_names if l not in layers_used]))
-    )
+    print("All layer ids:")
+    pprint(sorted(layer_ids))
+    print()
+
+    duplicate_layer_sets = [
+        lset for lset in layersets.keys() if len(layersets[lset]) > 0
+    ]
+    if len(duplicate_layer_sets) > 0:
+        print(f"Duplicate layersets: {len(duplicate_layer_sets)}")
+        # for lset in duplicate_layer_sets:
+        #     print(f"Layerset: {lset}")
+        #     print(f"Scenes: {', '.join(layersets[lset])}\n")
+
+    # print(
+    #     "Unused: {}".format(", ".join([l for l in layer_names if l not in layers_used]))
+    # )
 
 
 if __name__ == "__main__":
