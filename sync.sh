e113438662d2f8e8683aad3be09d814e2cd80f7c
Vincent Ahrend <mail@vincentahrend.com>
2019-12-03 11:57:50 +0100

    Remove rm from sync script

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
-aws s3 rm s3://fmb-aws-bucket/KatasterKI/scenes/ --recursive
+# aws s3 rm s3://fmb-aws-bucket/KatasterKI/scenes/ --recursive
 aws s3 sync out s3://fmb-aws-bucket/KatasterKI/scenes/ --exclude "*"  --include "*.jpg"
 aws s3 cp out/KatasterKI_scene_list.csv s3://fmb-aws-bucket/Data/KatasterKI_scene_list.csv
